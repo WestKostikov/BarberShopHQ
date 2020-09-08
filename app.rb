94ae8d95f97778b9473e5aaf5977c7acd5aac633
WestKostikov <maksrey@mail.ru>
2020-09-08 21:11:14 +0300

    Add Client entity

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, { adapter: 'sqlite3', database: 'barbershop.db' }
+
+class Client < ActiveRecord::Base
+end
+
 get '/' do 
     erb 'Hello!'
 end
