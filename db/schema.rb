16fa42e640324e499e2d9d6ce569422f08da3f75
WestKostikov <maksrey@mail.ru>
2020-09-08 21:28:55 +0300

    Add Barbers migration(1)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200908181437) do
+ActiveRecord::Schema.define(version: 20200908181920) do
+
+  create_table "barbers", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "clients", force: :cascade do |t|
     t.text     "name"
